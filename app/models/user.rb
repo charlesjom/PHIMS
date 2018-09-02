3864a7e3ccbf720fb56d17d1e4026f62cfa5520a
charlesjom <cumarquez@up.edu.ph>
2018-09-02 18:22:51 +0800

    Modified user model, added support for Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,22 @@
 class User
   include Mongoid::Document
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  ## Database authenticatable
+  field :email, type: String, default: ""
+  field :encrypted_password, type: String, default: ""
+  
+  ## Recoverable
+  field :reset_password_token,   type: String
+  field :reset_password_sent_at, type: Time
+
+  ## Rememberable
+  field :remember_created_at, type: Time
+
   field :username, type: String
-  field :email, type: String
   field :first_name, type: String
   field :middle_name, type: String
   field :last_name, type: String
