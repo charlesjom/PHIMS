eef8f71b15d60c71bb32ce43e4e49ce5e9fd892b
charlesjom <cumarquez@up.edu.ph>
2018-09-05 15:32:19 +0800

    authentication is made to be required in all pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations'}
   root to: 'users#index'
 
+  authenticated :user do
+    root to: 'home#index', as: :authenticated_root
+  end
+  root to: redirect('/users/sign_in')
+
   # get '/signup', to: 'users#new'
   # post '/signup', to: 'users#create'
 end
