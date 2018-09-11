f02d97db1b8243388b38655400ef7edfdb1e164f
charlesjom <cumarquez@up.edu.ph>
2018-09-11 13:25:40 +0800

    Added field for private and public key of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,17 +20,19 @@ class User
   field :first_name, type: String
   field :middle_name, type: String
   field :last_name, type: String
-  field :password_digest, type: String
   field :_id, type: String, default: ->{ email.to_s.downcase }, overwrite: true
+
+  # User keypair
+  field :encrypted_pri_key, type: String
+  field :pub_key, type: String
 
   before_validation(on: :create) do
     self.email = email.downcase
     self.id = email
   end
 
-  validates_presence_of :first_name, :middle_name, :last_name, :username, :email, :password
+  validates_presence_of :first_name, :middle_name, :last_name, :username, :email, :password, :pub_key, :encrypted_pri_key
   validates_format_of :first_name, :middle_name, :last_name, with: /\A[\w ]+\z/i
 	validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
-  validates_uniqueness_of :email
   validates :email, uniqueness: { case_sensitive: false }
 end
