c78b52dffb7849b412a2a8ad934c0e9ece1e6001
charlesjom <cumarquez@up.edu.ph>
2018-09-18 15:32:08 +0800

    Added validation for keypair, modified validation for email (user model)

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User
   ## Rememberable
   field :remember_created_at, type: Time
 
+  ## User Details
   field :username, type: String
   field :_id, type: String, default: ->{ email.to_s.downcase }, overwrite: true
 
@@ -23,7 +24,7 @@ class User
   field :middle_name, type: String
   field :last_name, type: String
 
-  # User keypair
+  ## User keypair
   field :encrypted_pri_key, type: String
   field :pub_key, type: String
 
@@ -33,8 +34,9 @@ class User
   end
 
   validates_presence_of :email, :password, :pub_key, :encrypted_pri_key
-  validates_presence_of :first_name, :middle_name, :last_name
+  validates_presence_of :first_name, :middle_name, :last_name, :username
+  validates_presence_of :encrypted_pri_key, :pub_key
   validates_format_of :first_name, :middle_name, :last_name, with: /\A[\w ]+\z/i
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
-  validates :email, uniqueness: { case_sensitive: false }
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates_uniqueness_of :email, :case_sensitive => false
 end
