8f47263f8800266a4e05ad99cb625e32025200ce
charlesjom <cumarquez@up.edu.ph>
2018-10-04 14:56:23 +0800

    Modified record model, add share key model

---
+++ app/models/share_key.rb
@@ -0,0 +1,11 @@
+class ShareKey
+  include Mongoid::Document
+  include Mongoid::Timestamps::Created
+
+  belongs_to: :record
+  belongs_to: :user
+
+  field :share_key, type: String
+
+  validates_presence_of :share_key
+end
