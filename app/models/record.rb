ff5d95379f313c6ff879ef05d09c4dfcc5dbfb33
charlesjom <cumarquez@up.edu.ph>
2018-10-04 14:56:23 +0800

    Modified record model, add share key model

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,12 +1,18 @@
 class Record
-  include Mongoid::Document
-  include Mongoid::Timestamps::Created
-  belongs_to :user
+	include Mongoid::Document
+	include Mongoid::Timestamps::Created
+	
+	## Associations with other models
+	has_many :share_keys
 
-  field :encrypted_file_key, type: String
-  # field for type of record
-  field :user_with_access, type: Array, default: []
-  field :storage_url, type: String
+	field :encrypted_file_key, type: String
+	field :storage_url, type: String
 
-  validates_presence_of :encrypted_file_key, :date_created, :user_with_access, :storage_url
+	validates_presence_of :encrypted_file_key, :date_created, :storage_url
+
+	# TODOS
+	# method for sharing records to other users
+	def generate_share_key(user_id, user_pub_key)
+		# algorithm for generating share_key
+	end
 end
