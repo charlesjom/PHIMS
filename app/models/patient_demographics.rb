bafd57f5d82abf708314e19efc6d68b476a02a81
charlesjom <cumarquez@up.edu.ph>
2018-10-09 17:19:23 +0800

    Made attribute choices into constants

--- app/models/patient_demographics.rb
+++ app/models/patient_demographics.rb
@@ -3,13 +3,23 @@ class PatientDemographic
     include ActiveModel::Validations
     include ActiveModel::Serializers::JSON
     
-    attr_accessor :sex, :birthdate, :civil_status, :street_address, :barangay, :city, :state_or_province, :country, :weight_in_kg, :height_in_cm, :abo_blood_type, :rh_type
+    attr_accessor :sex, :birthdate, :civil_status, :street_address, :barangay, :city, :state_or_province, :country, :weight_in_kg, :height_in_cm, :abo_blood_type, :rh_blood_type
+
+    SEX_CHOICES =  %w( M F )
+    SEX_CHOICES.freeze
+    CIVIL_STATUS_CHOICES = %w( Single Married Separated Widowed )
+    CIVIL_STATUSES.freeze
+    ABO_BLOOD_TYPES = %w( A B AB O Unknown )
+    ABO_BLOOD_TYPES.freeze
+    RH_BLOOD_TYPES = %w( + - Unknown )
+    RH_BLOOD_TYPES.freeze
 
     # validators for each attributes
-    validates_presence_of :sex, :civil_status, :birthdate, :street_address, :barangay, :city, :state_or_province, :country, :mass_in_kg, :height_in_cm, :abo_blood_type, :rh_type
-    validates_inclusion_of :sex, in: %w( M F )
-    validates_inclusion_of :civil_status, in: %w( Single Married Separated Widowed )
+    validates_presence_of :sex, :civil_status, :birthdate, :street_address, :barangay, :city, :state_or_province, :country, :mass_in_kg, :height_in_cm, :abo_blood_type, :rh_blood_type
+    validates_inclusion_of :sex, in: SEX_CHOICES
+    validates_inclusion_of :civil_status, in: CIVIL_STATUS_CHOICES
     validates_numericality_of :weight_in_kg, :height_in_cm
-    validates_inclusion_of :abo_blood_type, in: %w( A B AB O )
-    validates_inclusion_of :rh_type, in: %w( + - )
+    validates_inclusion_of :abo_blood_type, in: ABO_BLOOD_TYPES
+    validates_inclusion_of :rh_blood_type, in: RH_BLOOD_TYPES
+    validates_date :birthdate, :on_or_before => :today
 end
