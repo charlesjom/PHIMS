2656d5197c2cf29c54adeb17cd172fab7d5c5082
charlesjom <cumarquez@up.edu.ph>
2018-10-09 17:17:47 +0800

    Added date validation for attributes

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -9,7 +9,6 @@ class Insurance
     validates_presence_of :provider_name, :id_number, :valid_until, :name_of_insured, :birthdate_of_insured, :relationship_to_insured
     validates_numericality_of :id_number
     validates_format_of :telephone_number, with: \A\+?(\d{1,3})?-?(\d{3})?-?(\d{3}-?\d{4})\z, :if => :telephone_number_given?
-    # TODO validation for valid_until, birthdate_of_insured
-    # :valid_until should be after today
-    # :birthday_of_insured should be before today
+    validates_date :valid_until, :after => :today, :is_at => :today, :is_at_message => "Your insurance is expiring today"
+    validates_date :birthdate_of_insured, :on_or_before => :today
 end
