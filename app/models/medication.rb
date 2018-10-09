ce0263632a6efceff26c0237cff6b460f7ff095d
charlesjom <cumarquez@up.edu.ph>
2018-10-09 17:18:13 +0800

    Modified model name and added validation for date attributes

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,14 +1,15 @@
-class Allergy
+class Medication
     include ActiveModel::Model
     include ActiveModel::Validations
     include ActiveModel::Serializers::JSON
 
-    attr_accessor :medicine_name, :dosage, :frequency, :active, :date_modified
+    attr_accessor :medicine_name, :dosage, :frequency, :still_active, :date_modified
     
     # validators for each attributes
-    validates_presence_of :medicine_name, :dosage, :frequency, :active, :date_modified
+    validates_presence_of :medicine_name, :dosage, :frequency, :still_active, :date_modified
     validates_inclusion_of :active, in: [true, false]
-    
-    # TODO validation for date_modified
-    # should be before today
+
+    before_validation(on: :create, :update) do
+		self.date_modified = Date.current
+	end
 end
