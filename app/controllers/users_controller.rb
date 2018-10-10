81d822c31b43035c24283cc263b08edc4b1a15a9
charlesjom <cumarquez@up.edu.ph>
2018-10-10 16:04:21 +0800

    Fixed users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 class UsersController < ApplicationController
     # only authenticated users can access
-    before_action :authenticate_user!
 
     # GET /user/:user_id
     def show
-        raise
         @records = current_user
     end
 end
