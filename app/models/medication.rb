fac605732719e0390a296c7bf2879c5aef51d411
charlesjom <cumarquez@up.edu.ph>
2018-10-10 16:03:11 +0800

    Fixed method of medication model

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,6 +1,7 @@
 class Medication
     include ActiveModel::Model
     include ActiveModel::Validations
+    include ActiveModel::Validations::Callbacks
     include ActiveModel::Serializers::JSON
 
     attr_accessor :medicine_name, :dosage, :frequency, :still_active, :date_modified
@@ -9,7 +10,9 @@ class Medication
     validates_presence_of :medicine_name, :dosage, :frequency, :still_active, :date_modified
     validates_inclusion_of :active, in: [true, false]
 
-    before_validation(on: :create, :update) do
-		self.date_modified = Date.current
-	end
+    before_validation :set_date_modified, on: [:create, :update]
+    
+    def set_date_modified
+        self.date_modified = Date.current
+    end
 end
