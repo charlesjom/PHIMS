7766664b3fa8916017798a35c4b142e7915bdb15
charlesjom <cumarquez@up.edu.ph>
2018-10-10 15:59:29 +0800

    Fixed regex for telephone and cellphone numbers

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -8,7 +8,7 @@ class Insurance
     # validators for each attributes
     validates_presence_of :provider_name, :id_number, :valid_until, :name_of_insured, :birthdate_of_insured, :relationship_to_insured
     validates_numericality_of :id_number
-    validates_format_of :telephone_number, with: \A\+?(\d{1,3})?-?(\d{3})?-?(\d{3}-?\d{4})\z, :if => :telephone_number_given?
+    validates_format_of :telephone_number, with: /\A\+?(\d{1,3})?-?(\d{3})?-?(\d{3}-?\d{4})\Z/i, :if => :telephone_number_given?
     validates_date :valid_until, :after => :today, :is_at => :today, :is_at_message => "Your insurance is expiring today"
     validates_date :birthdate_of_insured, :on_or_before => :today
 end
