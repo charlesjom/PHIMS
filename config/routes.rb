3ea9a91aecc15808e9ad62d51fdeaa65d55fc70a
charlesjom <cumarquez@up.edu.ph>
2018-10-10 16:03:33 +0800

    Fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, :path => 'accounts', :controllers => { registrations: 'users/registrations'}
 
-  authenticated :user do
-    root to: 'user#show', as: :authenticated_root
+  authenticated :users do
+    root to: 'users#show', as: :authenticated_root
   end
   root to: redirect('/users/sign_in')
 
-  resources :users, only: :show do
-    resource :medical_history
-    resource :personal_data
-  end
+  resources :users, only: :show
+  resource :medical_history
+  resource :personal_data
 end
