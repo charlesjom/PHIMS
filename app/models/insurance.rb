8db79274d2ee0c479b81abb046e6731c9d4ef11a
charlesjom <charlesmarquez.27@gmail.com>
2019-02-21 03:51:41 +0800

    Modify model validations and remove file management- and encryption-related methods from model

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -3,12 +3,12 @@ class Insurance
     include ActiveModel::Validations
     include ActiveModel::Serializers::JSON
 
-    attr_accessor :provider_name, :id_number, :valid_until, :name_of_insured, :birthdate_of_insured, :relationship_to_insured, :telephone_number
+    attr_accessor :provider_name, :id_number, :valid_until, :name_of_insured, :birthdate_of_insured, :relationship_of_beneficiary_to_insured, :telephone_number
 
     # validators for each attributes
-    validates_presence_of :provider_name, :id_number, :valid_until, :name_of_insured, :birthdate_of_insured, :relationship_to_insured
-    validates_numericality_of :id_number
-    validates_format_of :telephone_number, with: /\A\+?(\d{1,3})?-?(\d{3})?-?(\d{3}-?\d{4})\Z/i, :if => :telephone_number_given?
-    validates_date :valid_until, :after => :today, :is_at => :today, :is_at_message => "Your insurance is expiring today"
-    validates_date :birthdate_of_insured, :on_or_before => :today
+    validates_presence_of :provider_name, :id_number, :valid_until, :name_of_insured, :birthdate_of_insured, :relationship_of_beneficiary_to_insured
+    with_options allow_blank: true do |v|
+        validates_format_of :telephone_number, with: /\A\+?(\d{1,3})?-?(\d{3})?-?(\d{3}-?\d{4})\Z/i
+        validates_date :birthdate_of_insured, :on_or_before => :today
+    end
 end
--- app/models/personal_data.rb
+++ app/models/personal_data.rb
@@ -1,7 +1,3 @@
-require 'aws-sdk'
-require "openssl"
-require "securerandom"
-
 class PersonalData
     extend ActiveModel::Naming
     include ActiveModel::Model
@@ -60,83 +56,5 @@ class PersonalData
 
     def self.lookup_ancestors
         [self]
-    end
-
-    ## Methods
-    def create_file
-        # TODOS
-        # 1. method for writing to JSON file
-        personal_data_in_json = self.to_json
-        file_name = self.owner.to_s + "_personal_data.json"
-        f = File.new(file_name, 'w')
-
-        # 2. encrypt file
-        # generate file_key, 32-byte base 64
-        file_key = SecureRandom.base64(32)
-        
-        # encrypt file using file_key
-        buffer = ""
-        File.open("#{file_name}.enc", "wb") do |outf|
-            File.open("#{file_name}", "rb") do |inf|
-                while inf.read(4096, buffer)
-                    outf << aes256_cipher_encrypt.update(buffer)
-                end
-                outf << aes256_cipher_encrypt.final
-            end
-        end
-
-        # get owner_public_key
-        owner_pub_key = self.owner.pub_key
-        # encrypt file_key using owner_public_key
-        encrypted_file_key = file_key.to_pem(aes256_cipher_encrypt, owner_pub_key)
-        encrypted_file_name = file_name + ".enc"
-
-        # 3. upload encrypted file to storage
-        s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
-        obj = s3.bucket('phims-uplb').object(encrypted_file_name)
-        obj.upload_file("#{self.owner.to_s}/#{encrypted_file_name}")
-
-        # 4. create a record model
-        if (storage_url != nil) and (encrypted_file_key != nil)
-            # create record model
-            @record = Record.create(
-                encrypted_file_key: encrypted_file_key,
-                storage_url: storage_url
-            )
-            return true # if upload succeeded
-        else
-            return false # if upload failed
-        end
-    end
-
-
-    def read_file
-        # get encrypted_filename
-        buf = ""
-        File.open("#{file_name}.enc", "wb") do |outf|
-            File.open("#{file_name}", "rb") do |inf|
-                while inf.read(4096, buf)
-                    outf << aes256_cipher_decrypt.update(buf)
-                end
-                outf << aes256_cipher_decrypt.final
-        end
-    end
-    
-    private
-        def aes256_cipher_encrypt
-            cipher = OpenSSL::Cipher::AES256.new(:CBC)
-            cipher.encrypt # encryption mode
-            key = cipher.random_key
-            iv = cipher.random_iv
-            return cipher
-        end
-
-        def aes256_cipher_decrypt
-            cipher = OpenSSL::Cipher::AES256.new(:CBC)
-            cipher.decrypt # decryption mode
-            key = cipher.random_key
-            iv = cipher.random_iv
-            return cipher
-        end
     end
 end
