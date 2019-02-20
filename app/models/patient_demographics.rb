474ab937c1af5ba380bdc07fd6e723de99f90f91
charlesjom <charlesmarquez.27@gmail.com>
2019-02-21 03:51:41 +0800

    Modify model validations and remove file management- and encryption-related methods from model

--- app/models/patient_demographics.rb
+++ app/models/patient_demographics.rb
@@ -5,21 +5,18 @@ class PatientDemographics
     
     attr_accessor :sex, :birthdate, :civil_status, :street_address, :barangay, :city, :state_or_province, :country, :weight_in_kg, :height_in_cm, :abo_blood_type, :rh_blood_type
 
-    SEX_CHOICES =  %w( M F )
-    SEX_CHOICES.freeze
-    CIVIL_STATUS_CHOICES = %w( Single Married Separated Widowed )
-    CIVIL_STATUS_CHOICES.freeze
-    ABO_BLOOD_TYPES = %w( A B AB O Unknown )
-    ABO_BLOOD_TYPES.freeze
-    RH_BLOOD_TYPES = %w( + - Unknown )
-    RH_BLOOD_TYPES.freeze
+    SEX_CHOICES = %w( Male Female Intersex ).freeze
+    CIVIL_STATUS_CHOICES = %w( Single Married Separated Widowed ).freeze
+    ABO_BLOOD_TYPES = %w( A B AB O ).freeze
+    RH_BLOOD_TYPES = %w( + - )freeze
 
-    # validators for each attributes
-    validates_presence_of :sex, :civil_status, :birthdate, :street_address, :barangay, :city, :state_or_province, :country, :mass_in_kg, :height_in_cm, :abo_blood_type, :rh_blood_type
-    validates_inclusion_of :sex, in: SEX_CHOICES
-    validates_inclusion_of :civil_status, in: CIVIL_STATUS_CHOICES
-    validates_numericality_of :weight_in_kg, :height_in_cm
-    validates_inclusion_of :abo_blood_type, in: ABO_BLOOD_TYPES
-    validates_inclusion_of :rh_blood_type, in: RH_BLOOD_TYPES
-    validates_date :birthdate, :on_or_before => :today
+    with_options allow_blank: true do |v|
+        v.validates_inclusion_of :sex, in: SEX_CHOICES
+        v.validates_inclusion_of :civil_status, in: CIVIL_STATUS_CHOICES
+        v.validates_numericality_of :weight_in_kg, :height_in_cm
+        v.validates_inclusion_of :abo_blood_type, in: ABO_BLOOD_TYPES
+        v.validates_inclusion_of :rh_blood_type, in: RH_BLOOD_TYPES
+        v.validates_date :birthdate, :on_or_before => :today
+    end
+    validates_presence_of :abo_blood_type, :if => lambda { self.rh_blood_type.present? }
 end
