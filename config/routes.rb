26e90fc0691e89bdd458a63a04bf0c015b416eed
charlesjom <charlesmarquez.27@gmail.com>
2019-03-12 02:06:09 +0800

    Change other devise-related pages
    Use haml as default template engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users, :path => 'accounts', :controllers => { registrations: 'users/registrations'}
+  devise_for :users, path: 'accounts', controllers: { registrations: 'users/registrations'}
 
+  unauthenticated do
+    root to: redirect('/users/sign_in')
+  end
+ 
   authenticated :users do
     root to: 'users#show', as: :authenticated_root
   end
-  root to: redirect('/users/sign_in')
+
+  get 'user/me', to: 'users#show'
 
   resources :users, only: :show
   resource :medical_history
