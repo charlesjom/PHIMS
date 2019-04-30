37d0445c8d457e4dade7de0b694cded26cd864ce
charlesjom <charlesmarquez.27@gmail.com>
2019-04-30 17:24:08 +0800

    Change UserRecord model
    Change read_file method
    Add user reference

--- app/models/user_record.rb
+++ app/models/user_record.rb
@@ -1,13 +1,14 @@
 class UserRecord < ApplicationRecord
     has_one_attached :file, dependent: :destroy
     has_many :share_keys
+    belongs_to :user
 
     validates_presence_of :encrypted_file_key, :encrypted_cipher_key, :encrypted_cipher_iv, :phr_type
 
-    def read_file(user_id = nil)
-        return if user_id.nil?
-        downloader = FileDownloader.new(self, user_id)
-        output_file = downloader.process
-        return
+    def read_file(user, password = nil)
+        return if user.nil? || password.nil?
+        downloader = FileDownloader.new(self, user)
+        output_file = downloader.process(password)
+        return output_file
     end
 end
