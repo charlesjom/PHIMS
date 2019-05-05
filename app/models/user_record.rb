d4702f556e225a12925e4a756050787d7a934600
charlesjom <charlesmarquez.27@gmail.com>
2019-05-06 03:18:15 +0800

    Add enumerize for phr_type and localization for values

--- app/models/user_record.rb
+++ app/models/user_record.rb
@@ -1,22 +1,18 @@
 class UserRecord < ApplicationRecord
+    extend Enumerize
+
     has_one_attached :file, dependent: :destroy
     has_many :share_keys
     belongs_to :user
 
     validates_presence_of :encrypted_file_key, :encrypted_cipher_key, :encrypted_cipher_iv, :phr_type
 
-    before_destroy :remove_files
+    enumerize :phr_type, in: [:medical_history, :personal_data], i18n_scope: "phr_types", scope: true, predicates: true
 
-    def read_file(user, password = nil)
+    def read_file(user = nil, password = nil)
         return if user.nil? || password.nil?
         downloader = FileDownloader.new(self, user)
         output_file = downloader.process(password)
         return output_file
-    end
-
-    private
-
-    def remove_files
-        file.purge
     end
 end
