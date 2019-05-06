efdcd5bd1754d63421d72a2463ccbf6d77f693b6
charlesjom <charlesmarquez.27@gmail.com>
2019-05-06 21:15:21 +0800

    Make wicked_pdf config dynamic based on environment

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -13,7 +13,7 @@ WickedPdf.config = {
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
-  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+  exe_path: (Rails.env.production? ? '/usr/local/bin/wkhtmltopdf' : `which wkhtmltopdf`)
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
