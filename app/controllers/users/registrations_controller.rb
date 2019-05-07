e6d7a230e5ad55584c8e1e87aeb6abc11084c341
charlesjom <charlesmarquez.27@gmail.com>
2019-05-07 23:44:43 +0800

    Delete unnecessary methods from registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,21 +14,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 	end
 
 	private
-		def aes256_cipher_encrypt
-		cipher = OpenSSL::Cipher::AES256.new(:CBC)
-		cipher.encrypt # encryption mode
-		key = cipher.random_key
-		iv = cipher.random_iv
-		return cipher
-	end
-
-	def aes256_cipher_decrypt
-		cipher = OpenSSL::Cipher::AES256.new(:CBC)
-		cipher.decrypt # decryption mode
-		key = cipher.random_key
-		iv = cipher.random_iv
-		return cipher
-	end
 
 	def sign_up_params
 		params.require(:user).permit(:first_name, :middle_name, :last_name, :username, :email, :password, :password_confirmation)
