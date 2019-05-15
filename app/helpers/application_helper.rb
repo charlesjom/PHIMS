750547351a444c04bc0c915e1e286263818dd48a
charlesjom <charlesmarquez.27@gmail.com>
2019-05-15 21:58:10 +0800

    Move flash types to controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-    def flash_class(key)
-        case key
-        when 'alert'
-            'warning'
-        when 'notice'
-            'info'
-        when 'error'
-            'error'
-        end
-    end
 end
