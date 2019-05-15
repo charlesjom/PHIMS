395ee43f19192e092b496c2cfda4d8925fdb04d4
charlesjom <charlesmarquez.27@gmail.com>
2019-05-15 22:02:57 +0800

    Restrict user_record CRUD functions to owner and user with access

--- app/controllers/user_records_controller.rb
+++ app/controllers/user_records_controller.rb
@@ -1,14 +1,26 @@
 class UserRecordsController < ApplicationController
-    before_action :set_user_record, only: [:show, :view, :destroy, :share, :share_form]
+    before_action :set_user_record, only: [:show, :view, :destroy, :edit, :share, :share_form, :edit_data]
 
     def index
         @user_records = current_user.user_records.to_a || []
         (@user_records << current_user.records_with_access.to_a).flatten!
     end
 
+    def show
+        if @user_record.is_owner?(current_user)
+            @share_keys = @user_record.share_keys
+        end
+    end
+
     def view
         output = @user_record.read_file(current_user, user_record_params)
-        send_file(output, type: 'application/pdf', disposition: 'inline')
+        if @user_record.errors.empty?
+            File.open(output, 'r') do |f|
+                send_data(f.read.force_encoding('BINARY'), type: 'application/pdf', disposition: 'inline')
+            end
+        else
+            redirect_to user_record_path(@user_record), error: @user_record.errors.full_messages.join(". ")
+        end
     end
 
     def destroy
@@ -19,16 +31,25 @@ class UserRecordsController < ApplicationController
     end
 
     def share
-        status = @user_record.share_file(current_user, user_record_params)
+        status = @user_record.share_file(current_user, share_user_record_params)
     end
-    
+
     def edit
     end
 
+    def edit_data
+        @output = @user_record.read_file(current_user, user_record_params)
+    end
+    
     private
 
     def set_user_record
-        @user_record = UserRecord.find(params[:id])
+        user_record = UserRecord.find(params[:id])
+        if user_record.has_access?(current_user)
+            @user_record = UserRecord.find(params[:id])
+        else
+            redirect_to root_path, warning: 'You do not have permission to access that record'
+        end
     end
     
     def user_record_params
